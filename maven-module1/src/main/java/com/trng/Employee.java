ab5547fcc19f653bc1af8ba76264378aefccb0bf
divyanshuyadav2003 <ydivyanshu2003@gmail.com>
2023-09-23 14:13:35 +0530

    This is initial project commit by me

---
+++ maven-module1/src/main/java/com/trng/Employee.java
@@ -0,0 +1,6 @@
+package com.trng;
+ import lambok.Getter;
+
+public class Employee {
+    private String empName;
+}
---
+++ maven-module2/src/main/java/com/trng/EmployeeDemo.java
@@ -0,0 +1,8 @@
+package com.trng;
+
+public class EmployeeDemo {
+    public static void main(String[] args)
+    {
+        Employee employee=new Employee();
+    }
+}
